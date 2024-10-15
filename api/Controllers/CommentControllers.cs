47c08a9c8097d628c8c76634d9665d70eb621823
CẦU HOÀNG VĂN <hoangcau14783@gmail.com>
2024-10-16 00:11:22 +0700

    #16-feat: Build comment infrastructure

---
+++ api/Controllers/CommentControllers.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentControllers : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        public CommentControllers(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+
+            var commentDTO = comments.Select(x=> x.ToCommentDTO());
+
+            return Ok(commentDTO);
+        }
+    }
+}
