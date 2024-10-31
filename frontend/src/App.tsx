12f1d5997bc8b247f8b4686d847bbabb63078a2d
CẦU HOÀNG VĂN <hoangcau14783@gmail.com>
2024-10-31 23:38:42 +0700

    refactor: Move the existing code from the old frontend folder to the author-code branch and recreate the new frontend to code from scratch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
-import { Outlet } from "react-router";
-import Navbar from "./Components/Navbar/Navbar";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from "./Context/useAuth";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <UserProvider>
-        <Navbar />
-        <Outlet />
-        <ToastContainer />
-      </UserProvider>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
